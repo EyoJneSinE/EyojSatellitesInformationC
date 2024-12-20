de9543dd6d5bd45bc6b5bb0ed7565dd665d01369
Enis Kaner <enis.d.kaner@gmail.com>
2024-12-20 16:21:49 +0300

    Update: MainActivity full screen mode

--- composeApp/src/androidMain/kotlin/com/eniskaner/eyojsatellitesinformation/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/eniskaner/eyojsatellitesinformation/MainActivity.kt
@@ -3,13 +3,27 @@ package com.eniskaner.eyojsatellitesinformation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.eniskaner.eyojsatellitesinformation.app.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.apply {
+            hide(WindowInsetsCompat.Type.systemBars())
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             App()
--- composeApp/src/androidMain/kotlin/com/eniskaner/eyojsatellitesinformation/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/eniskaner/eyojsatellitesinformation/Previews.kt
@@ -3,17 +3,17 @@ package com.eniskaner.eyojsatellitesinformation
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.eniskaner.eyojsatellitesinformation.satellite.satellites.domain.SatelliteUIModel
+import com.eniskaner.eyojsatellitesinformation.satellite.satellitedetail.presentation.SatelliteDetailScreen
+import com.eniskaner.eyojsatellitesinformation.satellite.satellitedetail.presentation.model.SatelliteDetailUIModel
+import com.eniskaner.eyojsatellitesinformation.satellite.satellitedetail.presentation.state.SatelliteDetailState
+import com.eniskaner.eyojsatellitesinformation.satellite.satellites.domain.model.SatelliteUIModel
 import com.eniskaner.eyojsatellitesinformation.satellite.satellites.presentation.SatelliteListScreen
-import com.eniskaner.eyojsatellitesinformation.satellite.satellites.presentation.components.SatelliteListItem
 import com.eniskaner.eyojsatellitesinformation.satellite.satellites.presentation.components.SatelliteSearchBar
 import com.eniskaner.eyojsatellitesinformation.satellite.satellites.presentation.state.SatelliteListUIState
-import com.eniskaner.eyojsatellitesinformation.satellite.satellites.presentation.state.satellites
 
 @Preview
 @Composable
@@ -33,6 +33,13 @@ private fun SatelliteSearchBarPreview() {
     }
 }
 
+val satellites = (1..40).map {
+    SatelliteUIModel(
+        id = it,
+        isActive = false,
+        name = "Starship-1"
+    )
+}
 
 
 @Preview
@@ -41,6 +48,27 @@ private fun SatelliteListScreenPreview() {
     SatelliteListScreen(
         state = SatelliteListUIState(
             searchingSatelliteList = satellites
+        ),
+        onAction = {}
+    )
+}
+
+@Preview
+@Composable
+private fun SatelliteDetailScreenPreview() {
+    val mock = SatelliteDetailUIModel(
+        id = 1,
+        name = "Starship - 1",
+        costPerLaunch = 8300000L,
+        firstFlight = "2020-10-03",
+        height = 110,
+        mass = 1135000,
+        posX = 0.213733842,
+        posY = 0.239480213
+    )
+    SatelliteDetailScreen(
+        state = SatelliteDetailState(
+            satelliteDetail = mock
         ),
         onAction = {}
     )
